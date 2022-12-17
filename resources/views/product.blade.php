ea3c928fbb222acf6b8a8a0dc48390e7eba9010d
farrelarrizal <mfarrel1301@gmail.com>
2022-12-17 13:10:13 +0700

    fixing product services

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -49,6 +49,10 @@
                 <input type="hidden" name="price" value="{{$product['price']}}">
                 <input type="hidden" name="qty_stock" value="{{$product['stock']}}">
                 <input type="hidden" name="gambar_hp" value="{{$product['img']}}">
+                <input type="hidden" name="device_brand" value="{{$product['device_brand']}}">
+                <input type="hidden" name="device_storage" value="{{$product['device_storage']}}">
+                <input type="hidden" name="device_connectivity" value="{{$product['device_connectivity']}}">
+                <input type="hidden" name="color" value="{{$product['color']}}">
                 <div class="w-48">
                     <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1 ">
                         <button type="button"   data-action="decrement" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
